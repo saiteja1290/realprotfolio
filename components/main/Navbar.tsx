5026954c5ffc58d229f56d5513861d694c68f191
saiteja.palegar@gmail.com <saiteja.palegar@gmail.com>
2024-04-16 13:50:49 +0530

    changes

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -1,21 +1,27 @@
 import { Socials } from "@/constants";
 import Image from "next/image";
 import React from "react";
-// import { CpuChipIcon } from '@heroicons/react/16/solid'
-import { CpuChipIcon } from "@heroicons/react/16/solid";
+
 const Navbar = () => {
     return (
         <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
             <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
-                {/* <a
+                <a
                     href="#about-me"
                     className="h-auto w-auto flex flex-row items-center"
                 >
-                    <CpuChipIcon className="text-gray-300" />
+                    <Image
+                        src="/NavLogo.png"
+                        alt="logo"
+                        width={70}
+                        height={70}
+                        className="cursor-pointer hover:animate-slowspin"
+                    />
+
                     <span className="font-bold ml-[10px] hidden md:block text-gray-300">
-                        Sai Teja
+                        WebChain Dev
                     </span>
-                </a> */}
+                </a>
 
                 <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
                     <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
@@ -31,18 +37,15 @@ const Navbar = () => {
                     </div>
                 </div>
 
-                <div className="flex flex-row gap-5 ">
+                <div className="flex flex-row gap-5">
                     {Socials.map((social) => (
-                        <a href={social.link}>
-                            <Image
-                                src={social.src}
-                                alt={social.name}
-                                key={social.name}
-                                width={24}
-                                height={24}
-                                className="filter brightness-0 invert"
-                            />
-                        </a>
+                        <Image
+                            src={social.src}
+                            alt={social.name}
+                            key={social.name}
+                            width={24}
+                            height={24}
+                        />
                     ))}
                 </div>
             </div>
