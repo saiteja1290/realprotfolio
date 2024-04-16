d8bbc5dae354aaaa0228152f993c31519323289c
saiteja.palegar@gmail.com <saiteja.palegar@gmail.com>
2024-04-16 14:07:35 +0530

    dumb extensions

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -13,19 +13,19 @@ const Projects = () => {
             </h1>
             <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
                 <ProjectCard
-                    src="/gans.png"
+                    src="/gans.PNG"
                     title="Synthetic Radar Data Generation using Generative Adversarial Networks"
                     description="Implemented GANs to enhance radar signal classification, which
                     generated synthetic radar data to augment the training dataset.
                     "
                 />
                 <ProjectCard
-                    src="/Neuralnexuswebsite.png"
+                    src="/Neuralnexuswebsite.PNG"
                     title="Neural Nexus Community Website"
                     description="Developed a website for a Neural Nexus CBIT club, it is being used and accessed by 500+ people."
                 />
                 <ProjectCard
-                    src="/NeuralChef.png"
+                    src="/NeuralChef.PNG"
                     title="Neural Chef - Competitive Coding Platform"
                     description="Designed and Developed a website where users can articipate in weekly
                     coding contests."
